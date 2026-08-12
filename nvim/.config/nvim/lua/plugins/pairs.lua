1b6788875e596f3fa48a4aeaf131cbeb6761983a
Thomas Wiebe <thomas@heimblick.net>
2026-08-12 17:37:54 +0200

    nvim: disable auto pairing

---
+++ nvim/.config/nvim/lua/plugins/pairs.lua
@@ -0,0 +1,5 @@
+-- Auto-pairing is off: typing an opening bracket or quote never inserts the
+-- closing one. Closing characters are always typed by hand.
+return {
+  { "echasnovski/mini.pairs", enabled = false },
+}
